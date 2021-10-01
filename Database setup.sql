e66ea149aecb2b499ba4684dc60e4a7de08890e1
Signum21 <matteo.gaeta.1998@gmail.com>
2021-10-01 16:57:35 +0200

    Changed query order

--- Database setup.sql
+++ Database setup.sql
@@ -1,5 +1,18 @@
 CREATE DATABASE sitoristoranti;
 
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL,
+  `Nome` varchar(30) NOT NULL,
+  `Cognome` varchar(30) NOT NULL,
+  `DataNascita` date NOT NULL,
+  `Genere` varchar(10) NOT NULL,
+  `Tipo` varchar(15) NOT NULL,
+  `Random` int(11) NOT NULL,
+  `Username` varchar(30) NOT NULL,
+  `Password` text NOT NULL,
+  `Attivo` tinyint(1) NOT NULL
+);
+
 CREATE TABLE `locali` (
   `Id` int(11) NOT NULL,
   `UserId` int(11) DEFAULT NULL,
@@ -27,13 +40,6 @@ CREATE TABLE `menu` (
   `Attivo` tinyint(1) NOT NULL
 );
 
-CREATE TABLE `menu_prenotazioni` (
-  `Id` int(11) NOT NULL,
-  `PrenotazioneId` int(11) DEFAULT NULL,
-  `MenuId` int(11) DEFAULT NULL,
-  `Quantita` int(11) NOT NULL
-);
-
 CREATE TABLE `prenotazioni` (
   `Id` int(11) NOT NULL,
   `UserId` int(11) DEFAULT NULL,
@@ -44,19 +50,18 @@ CREATE TABLE `prenotazioni` (
   `Stato` varchar(50) NOT NULL
 );
 
-CREATE TABLE `users` (
+CREATE TABLE `menu_prenotazioni` (
   `Id` int(11) NOT NULL,
-  `Nome` varchar(30) NOT NULL,
-  `Cognome` varchar(30) NOT NULL,
-  `DataNascita` date NOT NULL,
-  `Genere` varchar(10) NOT NULL,
-  `Tipo` varchar(15) NOT NULL,
-  `Random` int(11) NOT NULL,
-  `Username` varchar(30) NOT NULL,
-  `Password` text NOT NULL,
-  `Attivo` tinyint(1) NOT NULL
+  `PrenotazioneId` int(11) DEFAULT NULL,
+  `MenuId` int(11) DEFAULT NULL,
+  `Quantita` int(11) NOT NULL
 );
 
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Random` (`Random`),
+  ADD UNIQUE KEY `Username` (`Username`);
+  
 ALTER TABLE `locali`
   ADD PRIMARY KEY (`Id`),
   ADD KEY `UserId` (`UserId`);
@@ -65,39 +70,34 @@ ALTER TABLE `menu`
   ADD PRIMARY KEY (`Id`),
   ADD KEY `menu_ibfk_1` (`LocaleId`);
   
-ALTER TABLE `menu_prenotazioni`
-  ADD PRIMARY KEY (`Id`),
-  ADD KEY `IdPrenotazione` (`PrenotazioneId`),
-  ADD KEY `IdMenu` (`MenuId`);
-  
 ALTER TABLE `prenotazioni`
   ADD PRIMARY KEY (`Id`),
   ADD KEY `UserId` (`UserId`),
   ADD KEY `LocaleId` (`LocaleId`);
   
-ALTER TABLE `users`
+ALTER TABLE `menu_prenotazioni`
   ADD PRIMARY KEY (`Id`),
-  ADD UNIQUE KEY `Random` (`Random`),
-  ADD UNIQUE KEY `Username` (`Username`);
-  
+  ADD KEY `IdPrenotazione` (`PrenotazioneId`),
+  ADD KEY `IdMenu` (`MenuId`);
+
+ALTER TABLE `users` MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `locali` MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `menu` MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `menu_prenotazioni` MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
-
 ALTER TABLE `prenotazioni` MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `users` MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `menu_prenotazioni` MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `locali` ADD CONSTRAINT `locali_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`) ON DELETE CASCADE;
 
 ALTER TABLE `menu` ADD CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`LocaleId`) REFERENCES `locali` (`Id`) ON DELETE CASCADE;
 
-ALTER TABLE `menu_prenotazioni`
-  ADD CONSTRAINT `menu_prenotazioni_ibfk_1` FOREIGN KEY (`PrenotazioneId`) REFERENCES `prenotazioni` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `menu_prenotazioni_ibfk_2` FOREIGN KEY (`MenuId`) REFERENCES `menu` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
-  
 ALTER TABLE `prenotazioni`
   ADD CONSTRAINT `prenotazioni_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `prenotazioni_ibfk_2` FOREIGN KEY (`LocaleId`) REFERENCES `locali` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  
+ALTER TABLE `menu_prenotazioni`
+  ADD CONSTRAINT `menu_prenotazioni_ibfk_1` FOREIGN KEY (`PrenotazioneId`) REFERENCES `prenotazioni` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `menu_prenotazioni_ibfk_2` FOREIGN KEY (`MenuId`) REFERENCES `menu` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
